fe0ee34ae39f7055421b17a845897480a787ed61
eastminnn <oh1334000@gmail.com>
2026-09-15 09:38:43 +0900

    feat: 공통 생성 및 수정 시간 처리 추가

--- src/main/java/com/yufesta/YufestaApplication.java
+++ src/main/java/com/yufesta/YufestaApplication.java
@@ -2,8 +2,10 @@ package com.yufesta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YufestaApplication {
 
 	public static void main(String[] args) {
